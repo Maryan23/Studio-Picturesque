9cea046977547af019269803964a95d972ec42f5
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-11-26 07:29:24 +0300

    Create first template

--- Picturesque/settings.py
+++ Picturesque/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'galleria',
     'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
--- galleria/views.py
+++ galleria/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to Picturesque')
+    return render(request,'welcome.html')
 
