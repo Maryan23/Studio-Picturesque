133e13155922f5f41a88374b383b7131fd6c2d3c
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-11-26 23:10:00 +0300

    Made first migrations after creating class photos

--- galleria/models.py
+++ galleria/models.py
@@ -3,3 +3,15 @@ from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
+class Picture(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=2000)
+#     location = models.ForeignKey(Location)
+#     category = models.ForeignKey(Category)
+
+#     def __str__(self):
+#         return self.name
+
+# class Location(models.Model):
+
