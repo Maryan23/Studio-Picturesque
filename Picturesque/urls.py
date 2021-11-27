d3979c5ccdd0c48d73000b702608866a1063a689
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-11-27 16:11:09 +0300

    Styled images

--- Picturesque/urls.py
+++ Picturesque/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url(r'^galleria/',include('galleria.urls')),
+    url(r'',include('galleria.urls')),
 ]
--- galleria/views.py
+++ galleria/views.py
@@ -4,9 +4,6 @@ import datetime as dt
 from .models import Pictures
 
 # Create your views here.
-def welcome(request):
-    return render(request,'welcome.html')
-
 def index(request):
     Picture = Pictures.objects.all()
     return render(request, 'all_art/index.html',{'Picture':Picture})
