6c611c32da921890656219cf252b85a8f3649bca
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-11-27 22:26:32 +0300

    Added category model

--- galleria/models.py
+++ galleria/models.py
@@ -7,9 +7,31 @@ class Pictures(models.Model):
     image = CloudinaryField('image')
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=2000)
+    category = models.ManyToManyField(Category)
 
     @classmethod
     def search_by_name(cls,search_term):
         galleria = cls.objects.filter(name__icontains=search_term)
         return galleria
+    
+    @classmethod
+    def get_all_pictures(cls):
+        pictures = Pictures.objects.all()
+        return pictures
 
+    def save_picture(self):
+        self.save()
+    def delete_picture(self):
+        my_obj = Pictures.objects.get(pk = id(1))
+        self.delete(my_obj)
+    def update_picture(self,name,description):
+        self.name = name,
+        self.description = description,
+        self.category = category
+        self.save()
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
