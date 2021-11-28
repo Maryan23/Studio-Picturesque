a1b13e13819aff6ab30500e68f0a13a26c2d5ece
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-11-28 15:35:42 +0300

    Add click image functionality

--- galleria/views.py
+++ galleria/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse,Http404
 import datetime as dt
-from .models import Pictures
+from .models import Pictures,Category,Location
 from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def index(request):
-    Picture = Pictures.objects.all()
+    Picture = Pictures.objects.all().order_by('-id')
     return render(request, 'all_art/index.html',{'Picture':Picture})
 
 def search_results(request):
@@ -19,7 +19,7 @@ def search_results(request):
         return render(request, 'all_art/search.html',{"message":message,"pictures": searched_pictures})
 
     else:
-        message = "You haven't searched for any term"
+        message = "You haven't searched for any category"
         return render(request, 'all_art/search.html',{"message":message})
 
 def pictures(request,pictures_id):
@@ -28,4 +28,12 @@ def pictures(request,pictures_id):
     except ObjectDoesNotExist:
         raise Http404()
     return render(request,"all_art/pictures.html", {"pictures":pictures})
+
+def location(request,location_id):
+    location = Location.objects.all()
+    # pictures = Pictures.objects.get(id=location_id)
+    # loc = Location.objects.get(id=location_id)
+    return render (request,'all_art/navbar.html',{'location': location})
+
+
 
