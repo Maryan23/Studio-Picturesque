022531f4af3d82ff864a2d06c2be845e3c8f0f23
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-11-29 00:16:53 +0300

    Correctly displayed images on modals page

--- galleria/views.py
+++ galleria/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse,Http404
+from django.http import Http404
 import datetime as dt
-from .models import Pictures,Category,Location
+from .models import Pictures,Location
 from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
