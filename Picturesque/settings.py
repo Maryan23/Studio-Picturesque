caea50b009e77c7768bda3577af3b2abc8318189
Maryann Mwikali <mwikali119@gmail.com>
2022-12-26 12:14:35 +0300

    Configure .env

--- Picturesque/settings.py
+++ Picturesque/settings.py
@@ -93,10 +93,10 @@ WSGI_APPLICATION = 'Picturesque.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD':config('PASSWORD'),
-        'HOST':config('HOST'),
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD':os.getenv('PASSWORD'),
+        'HOST':os.getenv('HOST'),
     }
 }
 
